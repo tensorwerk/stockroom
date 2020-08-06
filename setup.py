6459419a4c0505ab4468a8785ad6cdc999d8f155
hhsecond <sherin@tensorwerk.com>
2020-08-06 22:02:26 +0530

    pypi type to markdown

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + '\n\n',
+    long_description_content_type = 'text/markdown',
     include_package_data=True,
     keywords='stockroom',
     name='stockroom',
