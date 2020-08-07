215c938d2277ab3e645684e8198ec9057f01e476
hhsecond <sherin@tensorwerk.com>
2020-08-07 09:57:28 +0530

    import issue fix and changelog

--- stockroom/__init__.py
+++ stockroom/__init__.py
@@ -1,14 +1,12 @@
 from stockroom.core import StockRoom
 from stockroom.keeper import init_repo
-__all__ = ['StockRoom', 'init_repo', '__version__']
+__all__ = ['StockRoom', 'init_repo', '__version__', 'make_torch_dataset']
 
 
 try:
     from hangar.dataset import make_torch_dataset
 except ModuleNotFoundError:
-    pass
-else:
-    __all__.append('make_torch_dataset')
+    from hangar import make_torch_dataset
 
 
 __version__ = '0.2.1'
